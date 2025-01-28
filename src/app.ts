f6f473e8a48f20172e27346d841eae48828bac4e
larissadantier <larissa_dantier@hotmail.com>
2025-01-28 18:22:57 -0300

    chore: add more plus configurations

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,12 @@
 import fastify from 'fastify';
+import fastifyPostgres from '@fastify/postgres';
+
 import 'dotenv/config';
+import { env } from 'env';
 
 export const app = fastify();
+
+
+app.register(fastifyPostgres, {
+  connectionString: `postgres://${env.POSTGRES_USER}:${env.POSTGRES_PASSWORD}@db:${env.POSTGRES_PORT}/${env.POSTGRES_DB}`,
+})
--- src/env/index.ts
+++ src/env/index.ts
@@ -6,6 +6,12 @@ config();
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'production']).default('production'),
   PORT: z.coerce.number().default(3333),
+  POSTGRES_SERVER: z.string(),
+  POSTGRES_USER: z.string(),
+  POSTGRES_HOST: z.string(),
+  POSTGRES_PORT: z.string(),
+  POSTGRES_PASSWORD: z.string(),
+  POSTGRES_DB: z.string()
 });
 
 
